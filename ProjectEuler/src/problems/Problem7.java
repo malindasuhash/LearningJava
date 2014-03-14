4a450669c69b04b373f2b67e6b3cc12e458a66f9
malinda.karunaratne <malinda.karunaratne@easyjet.com>
2014-03-14 16:37:40 +0000

    Adding 7 and half 8

--- ProjectEuler/src/problems/Problem7.java
+++ ProjectEuler/src/problems/Problem7.java
@@ -9,11 +9,44 @@ public class Problem7 {
 
 	public static void main(String[] args)
 	{
-		
+		Test();
 	}
 	
 	private static void Test()
 	{
+		int primeCount = 0;
 		
-	}	
+		for (int i = 2; i <= Integer.MAX_VALUE; i++)
+		{
+			if (isPrime(i))
+			{
+				primeCount++;
+				System.out.println("Prime: (" + primeCount + "): " + i);
+				
+			}
+			
+			if (primeCount == 10001)
+			{
+				break;
+			}
+		}
+	}
+	
+	private static boolean isPrime(int input)
+	{
+		if (input == 1)
+			return true;
+		
+		for (int i = 2; i < input; i++)
+		{
+			int reminder = input % i;
+			
+			if (reminder == 0)
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
 }
